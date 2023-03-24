5c819e42026ab1886658e05f5917ce106de3af0a
taikool2121996 <taikool2121996@gmail.com>
2023-03-24 10:58:51 +0700

    test husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,11 @@ const { off } = require("process");
 
 module.exports = {
   extends: [
-    // 'airbnb',
     'prettier',
     'plugin:@typescript-eslint/recommended',
     'airbnb/hooks',
+    // Muốn cấu hình theo eslint của airbnb thì bật cái này
+    // 'airbnb',
   ],
   plugins: ['prettier', 'import'],
   rules: {
@@ -19,36 +20,7 @@ module.exports = {
     ],
 
     // custom rule
-    'import/no-unresolved': 0, // turn on errors for missing imports
-    // 'import/no-extraneous-dependencies': 0,
-    // 'import/no-duplicates': 0,
-    // 'import/import/extensions': 0,
-    // 'import/import/no-self-import': 0,
-    // 'import/import/no-relative-packages': 0,
-    // 'import/no-cycle': [2, { ignoreExternal: true }],
-    'react/jsx-filename-extension': [0, { extensions: ['.ts','.tsx'] }],
-    // 'react/react-in-jsx-scope': 'off',
-    // 'react/jsx-props-no-spreading': [
-    //   0,
-    //   { html: 'ignore', custom: 'ignore', explicitSpread: 'ignore' },
-    // ],
-    // 'no-use-before-define': 0,
-    // '@typescript-eslint/no-use-before-define': 2,
-    // 'import/order': [
-    //   2,
-    //   {
-    //     groups: [
-    //       'builtin',
-    //       'external',
-    //       'internal',
-    //       'parent',
-    //       'sibling',
-    //       'index',
-    //     ],
-    //     'newlines-between': 'always',
-    //   },
-    // ],
-    // 'import/newline-after-import': 1,
+    '@typescript-eslint/no-use-before-define': 0,
   },
   parserOptions: {
     project: './tsconfig.eslint.json',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
-const testhusky = 0;
+const testhusky1 = 0;
 
 export default function Home() {
   return (
